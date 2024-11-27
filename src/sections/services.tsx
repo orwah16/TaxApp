4a00ab5d1b4edc08ac58d6a833d114f5a032a1c4
orwah <orwah_wattad@hotmail.com>
2024-11-27 22:59:09 +0200

    Update services.tsx

--- src/sections/services.tsx
+++ src/sections/services.tsx
@@ -1,13 +1,13 @@
 import { useLocale } from '../LocaleContext';
 import {FormattedMessage} from "react-intl";
-import Service from '../components/Service';
+import Service from "../components/service"
 
 function services() {
 	const { locale, messages, switchLocale } = useLocale() || {};    // Now you can safely use locale, messages, and switchLocale
 
   return (
     <section>
-    <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20 dark:bg-white dark:text-gray-800">
+    <div className="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-10 dark:bg-white dark:text-gray-800">
 	<h2 className="mb-8 text-4xl font-bold leading-none text-center">  
 		<FormattedMessage id="services" defaultMessage="خدمة العائدات الضريبية" />
 	</h2>
